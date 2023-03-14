8e7a2338b5209cd49b344b2c847cea5a93288452
Karly-Dan <daniela.kar99@gmail.com>
2023-03-14 16:15:52 -0600

    se modifica el contenido de la pagina principal

--- pages/index.vue
+++ pages/index.vue
@@ -1,92 +1,93 @@
 <template>
-    <main style="margin: 0; font-family: Arial, Helvetica, sans-serif;">
-      <h1 style="  font-size: 25px; border-left: 7px solid darkorange; padding-left: 10px;">Gestor de Campaña</h1>
-      <div style="box-shadow: var(--shd,0 1px 4px rgba(0,0,0,.6)); position: relative; width: 80%; margin: auto; margin-top: 20px; height: 305px; text-align: center; background-color: #00467a; color: white;">
-        <div style="background-color: white;">
-          <img src="../assets/sello.jpeg" alt="imagen" style="height: auto; width: 120px;">
+  <main>
+    <header-vue />
+    <barra-vue />
+    <div class="contenedor-informativo">
+        <div class="texto-informativo">
+            <h1 class="titulo-informativo">EC0217.01 Impartición de cursos de formación del capital humano de manera presencial grupal</h1>
+            <p>Descripción del Estándar de Competencia:
+                El estándar de competencia impartición de cursos de formación del capital humano de manera presencial y grupal contempla las funciones sustantivas de preparar, conducir y evaluar cursos de capacitación. Preparar la sesión mediante la planeación de la sesión y la comprobación de la existencia y el funcionamiento de los recursos requeridos para la sesión. Conducir la sesión realizando el encuadre, desarrollo y cierre, empleando técnicas instruccionales y grupales que faciliten el proceso de aprendizaje. Evaluar el aprendizaje antes, durante y al final del curso, considerando la satisfacción de los participantes/capacitandos.
+                <br>
+                <br>
+                El presente EC se fundamenta en criterios rectores de legalidad, competitividad, libre acceso, respeto, trabajo digno y responsabilidad social.
+                <br>
+                <br>
+                El presente EC actualiza al EC0217 “Impartición de cursos de formación del capital humano de manera presencial grupal”, publicado en el Diario Oficial de la Federación el 6 de julio de 2012.
+            </p>
         </div>
-        <h1 style="font-family: Arial, Helvetica, sans-serif;">CPCH</h1>
-        <div style="background-color: white; height: 40px; text-align: center; cursor: pointer;">
-          <img src="../assets/facebook(1).png" alt="" style="padding: 7px;">
-          <img src="../assets/instagram.png" alt="" style="padding: 7px;">
-          <img src="../assets/whatsapp.png" alt="" style="padding: 7px;">
-          <img src="../assets/correo(1).png" alt="" style="padding: 7px;">
+        <div class="contenedor-i-p">
+            <img src="../assets/administradora.jpg" alt="imagen" class="imagen-principal">
         </div>
-      </div>
-      <div style="background-color: #f3f4ed; height: 100px; margin-top: -72px;"></div>
-      <div style="background-color: #f3f4ed; padding: 20px;">
-        <h1 style="margin: 0; font-size: 25px; text-align: center;">Centro de Evaluación Conocer</h1>
-        <p style="text-align: center;">Un Organismo Certificador es una persona moral, organización o institución pública o privada, unidad administrativa de alguna Dependencia, Entidad o su similar en los niveles de gobierno Federal, Estatal o Municipal, acreditada por el CONOCER para certificar las competencias de las personas, con base en Estándares de Competencia inscritos en el Registro Nacional de Estándares de Competencia, así como para acreditar, previa autorización del CONOCER, Centros de Evaluación y/o Evaluadores Independientes en uno o varios Estándares de Competencia, inscritos en el Registro Nacional de Estándares de Competencia, durante un periodo determinado.</p>
-      </div>
-      <div style="background-color: #f3f4ed; margin-bottom: 20px;">
-        <tarjeta
-        titulo="Un Organismo Certificador debe:"
+    </div>
+    <div class="div-button">
+        <button-a-vue
+        nombre="Registrate"
+        href="https://eco2017.cpcapitalhumano.com.mx/" target="_blank"
         />
-        <br>
-        <br>
-        <tarjetaLista
-        titulo2="Los Organismos Certificadores deben asegurar que la certificación de competencias que realizan"
-        sub="Cumpla con los siguientes principios rectores:"
-        />
-        <br>
-        <br>
-      </div>
-      <h1 style="height: 5px; background-color: darkorange; width: 70%; text-align: center; margin: auto;"></h1>
-      <div style=" text-align: center; padding: 10px">
-        <h1 style="font-size: 20px;">¿Cuáles son las principales funciones de un Organismo Certificador?</h1>
-        <p>CERTIFICAR LAS COMPETENCIAS DE LAS PERSONAS CON BASE EN LOS ESTÁNDARES DE COMPETENCIA INSCRITOS EN EL REGISTRO NACIONAL DE COMPETENCIAS EN LOS QUE SE ENCUENTREN ACREDITADOS.</p>
-      </div>
-      <img src="../assets/certificado.png" alt="" style="display: flex; margin: auto;"> 
-      <br>
-      <h1 style="height: 5px; background-color: darkorange; width: 70%; text-align: center; margin: auto;"></h1>
-      <br>
-      <div style="background-color: rgb(243, 244, 237); margin-bottom: 20px;">
-        <h2 style="font-size: 20px; text-align: center; padding-top: 10px;">¿Qué requisitos se deben cumplir para la acreditación inicial del Organismo Certificador?</h2>
-        <table>
-          <tr>
-            <td style= "border: 2px solid darkorange;">1. Solicitud de información para la Acreditación Inicial o por Primera Vez</td>
-            <td style= "border: 2px solid #00467a;">2. Solicitud de acreditación</td>
-          </tr>
-          <tr>
-            <td  style= "border: 2px solid #00467a;">3. Carta compromiso</td>
-            <td  style= "border: 2px solid darkorange;">4. Acta constitutiva o documentos legales</td>
-          </tr>
-          <tr>
-            <td  style= "border: 2px solid darkorange;">5. Identificación oficial del Representante Legal</td>
-            <td  style= "border: 2px solid #00467a;">6. Cédula de identificación fiscal</td>
-          </tr>
-          <tr>
-            <td  style= "border: 2px solid #00467a;">7. Comprobante del domicilio</td>
-            <td  style= "border: 2px solid darkorange;">8. Proyecto de participación en el Sistema Nacional de Competencias</td>
-          </tr>
-        </table>
-      </div>
-    </main>
-  </template>
-  
-  <script>
-  import tarjeta from '../components/tarjeta.vue';
-  import tarjetaLista from '../components/tarjetaLista.vue';
-  export default {
+    </div>
+    <footer-vue />
+  </main>
+</template>
+
+<script>
+import barraVue from '../components/barra.vue'
+import buttonAVue from '../components/buttonA.vue'
+import footerVue from '../components/footer.vue'
+import headerVue from '../components/header.vue'
+
+export default {
     components: {
-      tarjeta,
-      tarjetaLista,
-    }
-  }
-  </script>
-  <style>
-  body {
+        barraVue,
+        buttonAVue,
+        headerVue,
+        footerVue,
+    },
+}
+</script>
+ 
+<style>
+body {
     margin: 0;
-  }
-  table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  width: 95%;
-  margin: auto;
+}
+.lineas {
+    border-radius: 2px;
+    height: 2px;
+    background-color: black;
 }
 
-td, th {
-  text-align: left;
-  padding: 8px;
+
+
+@media (max-width: 776px) {
+  .texto-informativo {
+    width: 90%;
+    margin: auto;
+    text-align: justify;
+  }
+  .titulo-informativo {
+    text-align: initial;
+  }
+  .imagen-principal {
+    width: 350px;
+    margin-left: auto;
+    margin-right: auto;
+    display: block;
+  }
+  
 }
-  </style>
+
+@media (min-width: 1024px) {
+    .contenedor-informativo {
+        display: flex;
+        height: 436px;
+    }
+    .texto-informativo {
+        height: 311px;
+        width: 64%;
+        padding-left: 10px;
+    }
+    .imagen-principal {
+        margin-top: 23px;
+    }
+   
+}
+</style>
