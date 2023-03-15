71680bf1b320b58ab10f0c656fbed7d10a95152b
Karly-Dan <daniela.kar99@gmail.com>
2023-03-15 15:37:05 -0600

    se agrega configuracion

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,8 @@
 export default {
+  server: {
+    port: 3001 // default: 3000
+  },
+  
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Centro de Profesionalización de Capital Humano',
